d5fb77aebe6c5f345dd4d9174a10b42bb4ed8150
s-saito <s.s.saito108@gmail.com>
2023-01-26 19:38:30 +0900

    add blog

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import React, { FC } from 'react'
 import { getAllPostIds, getPostData } from '../../../lib/posts'
-import Date from '../../components/Date/date'
+import Date from '@/components/Date/date'
 
 type Props = {
   postData: {
@@ -30,23 +30,27 @@ export async function getStaticProps({ params }: { params: { id: string } }) {
   }
 }
 
+const blogTitleClass = 'mb-4 text-2xl font-bold leading-normal'
+const blogPostdDateClass = 'mb-8 text-xs text-gray-400'
+const blogContentClass = '[&>p]:mb-4 [&>p:last-child]:mb-0 leading-relaxed text-sm'
+const blogBackLinkClass = 'block mt-8 text-sm'
+
 const Post: FC<Props> = ({ postData }) => {
   return (
     <>
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <div>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
+      <h2 className={blogTitleClass}>{postData.title}</h2>
+      <p className={blogPostdDateClass}>
         <Date dateString={postData.date} />
-        <br />
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </div>
+      </p>
+      <div
+        className={blogContentClass}
+        dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+      />
       <Link href={`/blog`}>
-        <a>ブログトップへ戻る</a>
+        <a className={blogBackLinkClass}>←&nbsp;ブログトップへ戻る</a>
       </Link>
     </>
   )
